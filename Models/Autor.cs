67e8c170e2b10a24e2276bc815d4cf04a7fadf0f
Patrick Soethe <pat1515@live.de>
2020-12-18 20:45:28 +0100

    Packages hinzugefügt, Autor Klasse angepasst

--- Models/Autor.cs
+++ Models/Autor.cs
@@ -3,12 +3,13 @@ namespace MasterarbeitRestServer.Models
     public class Autor
     {
         public int ID { get; set; }
-        public string Vorname { get; set; }
-        public string Nachname { get; set; }
-        public int Alter { get; set; }
-        public int Groesse { get; set; }
-        public string Adresse { get; set; }
-        public string Ort { get; set; }
-        public string Land { get; set; }
+        public string VORNAME { get; set; }
+        public string NACHNAME { get; set; }
+        public int LEBENSALTER { get; set; }
+        public string ORT { get; set; }
+        public string LAND { get; set; }
+        public int GROESSE { get; set; }
+        //public string ADRESSE { get; set; }
+
     }
 }
